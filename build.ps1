04425fbcbf6f307534bc2a32fd54db249bdd45ec
harrison.nguyen <harrison.nguyen@luminetsoft.com>
2021-01-24 14:08:32 +0700

    add build script

---
+++ build.ps1
@@ -0,0 +1,113 @@
+write-host "`n  ## NODEJS INSTALLER ## `n"
+
+### CONFIGURATION
+
+# nodejs
+$version = "14.15.4-x64"
+$url = "https://nodejs.org/dist/latest-v14.x/node-v$version.msi"
+
+# extras
+$vsc_exe = "$PSScriptRoot\vsc.exe"
+$vsc_url = "https://go.microsoft.com/fwlink/?LinkID=623230"
+
+
+# activate / desactivate any install
+$install_node = $TRUE
+
+write-host "`n----------------------------"
+write-host " system requirements checking  "
+write-host "----------------------------`n"
+
+### require administator rights
+
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+   write-Warning "This setup needs admin permissions. Please run this file as admin."     
+   break
+}
+
+### nodejs version check
+
+if (Get-Command node -errorAction SilentlyContinue) {
+    $current_version = (node -v)
+}
+ 
+if ($current_version) {
+    write-host "[NODE] nodejs $current_version already installed"
+    $confirmation = read-host "Are you sure you want to replace this version ? [y/N]"
+    if ($confirmation -ne "y") {
+        $install_node = $FALSE
+    }
+}
+
+write-host "`n"
+if ($install_node) {
+    
+    ### download nodejs msi file
+    # warning : if a node.msi file is already present in the current folder, this script will simply use it
+        
+    write-host "`n----------------------------"
+    write-host "  nodejs msi file retrieving  "
+    write-host "----------------------------`n"
+
+    $filename = "node.msi"
+    $node_msi = "$PSScriptRoot\$filename"
+    
+    $download_node = $TRUE
+
+    if (Test-Path $node_msi) {
+        $confirmation = read-host "Local $filename file detected. Do you want to use it ? [Y/n]"
+        if ($confirmation -eq "n") {
+            $download_node = $FALSE
+        }
+    }
+
+    if ($download_node) {
+        write-host "[NODE] downloading nodejs install"
+        write-host "url : $url"
+        $start_time = Get-Date
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($url, $node_msi)
+        write-Output "$filename downloaded"
+        write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    } else {
+        write-host "using the existing node.msi file"
+    }
+
+    ### nodejs install
+
+    write-host "`n----------------------------"
+    write-host " nodejs installation  "
+    write-host "----------------------------`n"
+
+    write-host "[NODE] running $node_msi"
+    Start-Process $node_msi -Wait
+    
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+    
+} else {
+    write-host "Proceeding with the previously installed nodejs version ..."
+}
+
+### npm packages install
+
+write-host "`n----------------------------"
+write-host " npm packages installation  "
+write-host "----------------------------`n"
+
+write-host "Installing pm2"
+npm install --global pm2
+npm install pm2-windows-startup -g
+pm2-startup install
+
+write-host "`n----------------------------"
+write-host " build source  "
+write-host "----------------------------`n"
+
+$from = './'
+$to = '../container-photo-update-build'
+copy-item $from $to -Recurse -Force
+
+cd ../container-photo-update-build
+npm run build
+ 
+Get-ChildItem -Exclude dist | Remove-Item -Recurse -Force .\client\*
